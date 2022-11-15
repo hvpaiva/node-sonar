6f33ed110a22e286b8f6c40f1670d0bbc1ed4bec
Highlander Paiva <hvpaiva@icloud.com>
2022-11-15 15:53:53 -0300

    feat: created say bye function

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,11 @@
 const index = require('../src')
+
 test('Should say hello', () => {
     const result = index.sayHello("World");
     expect(result).toEqual("Hello World!");
+})
+
+test('Should say bye', () => {
+    const result = index.sayHello("World");
+    expect(result).toEqual("Bye, World!");
 })
